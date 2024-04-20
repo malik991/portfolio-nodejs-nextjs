aa8fdee71afe3eb90bb76a95bf799aeaf6acedcf
malik <mubi.emailid@gmail.com>
2024-04-20 18:48:49 +0500

    update internal and external navlinks

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -52,6 +52,7 @@ const HeroSection = () => {
               href="/cv/cv.pdf"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
               download
+              target="_blank"
             >
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
                 Download CV
--- src/app/components/NavLink.jsx
+++ src/app/components/NavLink.jsx
@@ -1,6 +1,21 @@
 import Link from "next/link";
 
 const NavLink = ({ href, title }) => {
+  // Check if the href starts with # to determine if it's an internal link
+  const isInternalLink = href.startsWith("#");
+
+  // If it's an internal link, just render an anchor tag
+  if (isInternalLink) {
+    return (
+      <a
+        href={href}
+        className="block py-2 pl-3 pr-4 text-[#ADB7BE] sm:text-xl rounded md:p-0 hover:text-white"
+      >
+        {title}
+      </a>
+    );
+  }
+  // If it's an external link, use Next.js's Link component
   return (
     <Link
       href={href}
