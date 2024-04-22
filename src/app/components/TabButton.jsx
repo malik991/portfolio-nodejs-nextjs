1eee2bc1fa6d2f1558523a705bb88f2f2e71ab5e
malik <mubi.emailid@gmail.com>
2024-04-22 14:59:10 +0500

    change tab colour of education

--- src/app/components/TabButton.jsx
+++ src/app/components/TabButton.jsx
@@ -19,7 +19,7 @@ const TabButton = ({ active, selectTab, children }) => {
       <motion.div
         animate={active ? "active" : "default"}
         variants={variants}
-        className="h-1 bg-primary-500 mt-2 mr-3"
+        className="h-1 bg-[#DC6B19] mt-2 mr-3"
       ></motion.div>
     </button>
   );
